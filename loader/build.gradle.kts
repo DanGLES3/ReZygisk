a6a4919e0e2dad9b6332d0d65e9b5d8474cb83fb
DanGLES3 <switchtegra@gmail.com>
2024-11-08 20:23:08 -0300

    Update build.gradle.kts
    Signed-off-by: DanGLES3 <switchtegra@gmail.com>

--- loader/build.gradle.kts
+++ loader/build.gradle.kts
@@ -33,7 +33,7 @@ val defaultCFlags = arrayOf(
 )
 
 val releaseFlags = arrayOf(
-    "-O3", "-flto=thin",
+    "-Ofast", "-flto=thin",
     "-Wno-unused", "-Wno-unused-parameter",
     "-fvisibility=hidden", "-fvisibility-inlines-hidden",
     "-fno-unwind-tables", "-fno-asynchronous-unwind-tables",
