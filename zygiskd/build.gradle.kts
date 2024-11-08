0a913c6ea4d432fe387249bb079b35ae359971ce
DanGLES3 <switchtegra@gmail.com>
2024-11-08 20:22:38 -0300

    Update build.gradle.kts
    Signed-off-by: DanGLES3 <switchtegra@gmail.com>

--- zygiskd/build.gradle.kts
+++ zygiskd/build.gradle.kts
@@ -38,7 +38,7 @@ val CStandardFlags = arrayOf(
 )
 
 val CFlagsRelease = arrayOf(
-  "-Wl,--strip-all", "-flto=thin", "-O3"
+  "-Wl,--strip-all", "-flto=thin", "-Ofast"
 )
 
 val CFlagsDebug = arrayOf(
